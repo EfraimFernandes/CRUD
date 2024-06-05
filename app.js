9a3f57e75827313de857079b122c675627997392
Efraim  Fernandes <efraim.fernandes@outlook.com>
2024-06-05 00:48:24 -0300

    Adicionando uma página para fazer a operação read

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const port = 3000;
 
 const usuariosDB = new sqlite3.Database('crud.db')
+
+app.set('view engine', 'ejs');
+
+// Rota para a página de lista de usuários
+app.get('/', (req, res) => {
+    usuariosDB.all(`SELECT * FROM usuarios`, (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Erro ao buscar dados do banco de dados");
+            return;
+        }
+        res.render('index', { usuarios: rows });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
 
 //Criar uma tabela dentro do banco de dados
 //usuariosDB.run('CREATE TABLE usuarios (nome TEXT, idade INTEGER, email TEXT )')
@@ -14,10 +35,10 @@ const usuariosDB = new sqlite3.Database('crud.db')
 //Deletar um usuário da tabela
 //usuariosDB.run(`DELETE FROM usuarios WHERE nome='Daniel'`)
 
-usuariosDB.all(`SELECT * FROM usuarios`, (err, rows) => {
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log(rows)
-})
+// usuariosDB.all(`SELECT * FROM usuarios`, (err, rows) => {
+//     if(err){
+//         console.log(err)
+//         return
+//     }
+//     console.log(rows)
+// })
